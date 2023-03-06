c6a7dffb384114aa30ceba40605648defbf32a19
Vishwajeet Thakur <vishwajeetthakur@Vishwajeets-MacBook-Pro.local>
2023-03-06 03:48:13 -0500

    more exception handled

--- create_iam_user.py
+++ create_iam_user.py
@@ -2,12 +2,19 @@ import boto3
 
 iam = boto3.client('iam')
 
-try:
-    response = iam.create_user(
+try: 
+    iam.get_user(
         UserName='vicky'
     )
-    print("User is created sucessfully ")
-except iam.exceptions.EntityAlreadyExistsException as e:
-    print("EntityAlreadyExistsException ", e)
+    print(f"user already exists")
+except iam.exceptions.NoSuchEntityException as e:
+    try:
+        iam.create_user(
+            UserName = "vicky"
+        )
+        print(f"user created")
+    except Exception as e:
+        print("some other exception details : ",e)
+
 
 print("****** Script Ended *******")
